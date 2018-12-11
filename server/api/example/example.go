2f97b8395e5f6c89e609d119afb4083574b415ba
bli940505 <ben@xage.com>
2018-12-10 21:31:13 -0800

    updated example

--- server/api/example/example.go
+++ server/api/example/example.go
@@ -1,6 +1,7 @@
 package example
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/bli940505/slackChan/server/internals/configs"
@@ -8,58 +9,65 @@ import (
 	"github.com/go-chi/render"
 )
 
+// Routes returns a router with all of the Example routes attached
 func Routes(configuration *config.Config) *chi.Mux {
 	router := chi.NewRouter()
-	router.Get("/{id}", GetSingleExample(configuration))
-	router.Delete("/{id}", DeleteExample(configuration))
-	router.Post("/", CreateExample(configuration))
+	router.Get("/{id}", GetExample(configuration))
 	router.Get("/", GetAllExamples(configuration))
+	router.Delete("/{id}", DeleteExample(configuration))
+	router.Post("/{id}", CreateExample(configuration))
 	return router
 }
 
+// Example represents a returned example
 type Example struct {
-	Slug  string `json:"slug"`
 	Title string `json:"title"`
 	Body  string `json:"body"`
 }
 
-func GetSingleExample(configuration *config.Config) http.HandlerFunc {
+// GetExample returns a single example by id
+func GetExample(configuration *config.Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id := chi.URLParam(r, "id")
 		example := Example{
-			Slug:  id,
-			Title: "Hello world from PORT: " + configuration.Constants.PORT,
-			Body:  "Heloo world from planet earth",
+			Title: "SlackChan running on PORT " + configuration.Constants.PORT,
+			Body:  fmt.Sprintf("ID you gave is %s", id),
 		}
 		render.JSON(w, r, example) // A chi router helper for serializing and returning json
 	}
 }
 
-func DeleteExample(configuration *config.Config) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		response := make(map[string]string)
-		response["message"] = "Deleted Example successfully"
-		render.JSON(w, r, response) // Return some demo response
-	}
-}
-
-func CreateExample(configuration *config.Config) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		response := make(map[string]string)
-		response["message"] = "Created Example successfully"
-		render.JSON(w, r, response) // Return some demo response
-	}
-}
-
+// GetAllExamples returns all the examples stored in the database
 func GetAllExamples(configuration *config.Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		examples := []Example{
 			{
-				Slug:  "slug",
-				Title: "Hello world",
-				Body:  "Heloo world from planet earth",
+				Title: "SlackChan running on PORT " + configuration.Constants.PORT,
+				Body:  "I HAVE NEVER HAD SEX WITH WAYNE",
+			},
+			{
+				Title: "SlackChan running on PORT " + configuration.Constants.PORT,
+				Body:  "I HAVE NEVER HAD SEX WITH DRAKE",
 			},
 		}
-		render.JSON(w, r, examples) // A chi router helper for serializing and returning json
+		render.JSON(w, r, examples)
+	}
+}
+
+// DeleteExample deletes an example by id from the database
+func DeleteExample(configuration *config.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		response := make(map[string]string)
+		response["message"] = "Deleted Example successfully"
+		render.JSON(w, r, response)
+	}
+}
+
+// CreateExample creates an example by id
+func CreateExample(configuration *config.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		response := make(map[string]string)
+		response["message"] = "Created Example successfully"
+		render.JSON(w, r, response)
 	}
 }
